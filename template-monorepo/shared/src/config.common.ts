c59a8fe6d9949d687471521955bf7bd4966560f2
Vadim Laletin <rusmiligamer@gmail.com>
2020-11-09 20:16:23 +0300

    Add comments. Update fastfile

--- template-monorepo/shared/src/config.common.ts
+++ template-monorepo/shared/src/config.common.ts
@@ -1,1 +1,5 @@
+// Put common settings/constants for all environments
+// They will be accessible via `import config from 'shared/lib/config';` along with
+// environment settings
+
 export default {};
