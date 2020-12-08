bacb466222e9e333249c80d6f1c9c8ac37d8d991
Vadim Laletin <ruscoder@users.noreply.github.com>
2020-12-08 09:58:27 +0300

    Update .eslintrc.js

--- template-monorepo/.eslintrc.js
+++ template-monorepo/.eslintrc.js
@@ -5,9 +5,36 @@ module.exports = {
     root: true,
     extends: ['@react-native-community', 'prettier', 'prettier/react'],
     parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint', 'jest'],
+    plugins: ['@typescript-eslint', 'jest', 'import'],
     rules: {
         'react-native/no-inline-styles': 0,
         'no-restricted-imports': ['error', { patterns: ['shared/src', 'aidbox-react/src'] }],
+        'prettier/prettier': 0,
+        'import/order': [
+            'error',
+            {
+                groups: ['builtin', 'external', 'internal', ['index', 'sibling', 'parent']],
+                'newlines-between': 'always',
+                pathGroupsExcludedImportTypes: ['builtin'],
+                pathGroups: [
+                    {
+                        pattern: 'aidbox-react/**',
+                        group: 'external',
+                        position: 'after',
+                    },
+                    {
+                        pattern: 'shared/**',
+                        group: 'internal',
+                        position: 'after',
+                    },
+                    {
+                        pattern: 'src/**',
+                        group: 'internal',
+                        position: 'after',
+                    },
+                ],
+                alphabetize: { order: 'asc', caseInsensitive: true },
+            },
+        ],
     },
 };
