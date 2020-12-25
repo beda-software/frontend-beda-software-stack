c39348c84b1e3c4139c66f76313466fe12433c5b
Vadim Laletin <rusmiligamer@gmail.com>
2020-12-25 21:31:07 +0300

    Get rid of nohoist for react-native. Closes #18

--- template-mobile/template/metro.config.js
+++ template-mobile/template/metro.config.js
@@ -5,26 +5,26 @@
  * @format
  */
 
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-  resolver: {
-    extraNodeModules: new Proxy(
-      {},
-      {
-        get: (target, name) => {
-          return path.join(__dirname, `node_modules/${name}`);
-        },
-      }
-    ),
-  },
-  watchFolders: [path.resolve(__dirname, "../")],
+    transformer: {
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: false,
+            },
+        }),
+    },
+    resolver: {
+        extraNodeModules: new Proxy(
+            {},
+            {
+                get: (target, name) => {
+                    return path.join(__dirname, `../node_modules/${name}`);
+                },
+            },
+        ),
+    },
+    watchFolders: [path.resolve(__dirname, '../')],
 };
